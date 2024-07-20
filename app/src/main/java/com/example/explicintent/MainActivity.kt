3b2c58603c4d6c67deb00119854f2b1a8b474242
Sawan Kumar <sawankumarroy0@gmail.com>
2024-07-20 23:17:56 +0530

    Adding files to the projects

---
+++ app/src/main/java/com/example/explicintent/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.explicintent
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnIntent = findViewById<Button>(R.id.button)
+
+        btnIntent.setOnClickListener{
+            // open a new activity
+            intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
